2f59c1254dd4f4bff549b9798d4afa823aa7650b
Jacopo <jakonigro@gmail.com>
2020-10-20 16:03:03 +0200

    creato documento in config con API

--- config/pasta.php
+++ config/pasta.php
@@ -1,5 +1,4 @@
-<!-- API -->
-@php
+<?php
  $data = '[
   {
     "src": "https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg",
@@ -124,71 +123,4 @@
   ]';
 
   $data = json_decode($data, true);
-  $lunga = [];
-  $corta = [];
-  $cortissima = [];
-  foreach ($data as $pasta) {
-    if ($pasta["tipo"] == "lunga") {
-      $lunga[] = $pasta;
-    }
-    else if ($pasta["tipo"] == "corta") {
-      $corta[] = $pasta;
-    }
-    else if ($pasta["tipo"] == "cortissima") {
-      $cortissima[] = $pasta;
-    }
-  }
-@endphp
-<!-- /API -->
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>molisana</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-  <body>
-    <!-- header -->
-    <header>
-      <div class="logo-container">
-        <img class="logo" src="{{asset('img/logo.png')}}" alt="">
-      </div>
-      <div class="header-nav">
-        <ul class="nav-list">
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </header>
-    <!-- /header -->
-    <!-- main -->
-    <main>
-      <h2>Le Lunghe</h2>
-      <ul class="pasta-list">
-        <?php foreach ($lunga as $pasta): ?>
-          <li>
-            <img class="pasta-img" src="{{$pasta["src"]}}" alt="">
-          </li>
-        <?php endforeach; ?>
-      </ul>
-      <h2>Le Corte</h2>
-      <ul class="pasta-list">
-        <?php foreach ($corta as $pasta): ?>
-          <li>
-            <img class="pasta-img" src="{{$pasta["src"]}}" alt="">
-          </li>
-        <?php endforeach; ?>
-      </ul>
-      <h2>Le Cortissime</h2>
-      <ul class="pasta-list">
-        <?php foreach ($cortissima as $pasta): ?>
-          <li>
-            <img class="pasta-img" src="{{$pasta["src"]}}" alt="">
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </main>
-    <!-- /main -->
-  </body>
-</html>
+  return $data;
