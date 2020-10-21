07763e127d56b41bb7f661fdf0667ab2704991cf
Jacopo <jakonigro@gmail.com>
2020-10-21 15:46:58 +0200

    wip footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,5 +64,55 @@ foreach ($data as $pasta) {
       </ul>
     </main>
     <!-- /main -->
+    <!-- footer -->
+    <footer>
+      <div class="contacts-container">
+        <img src="" alt="">
+        <ul class="contacts">
+          <li>Ragione sociale: La Molisana S.P.A.<li>
+          <li>Sede legale: Contrada Colle delle Api, 100/A - 86100 - Campobasso (CB)<li>
+          <li>Pec: lamolisana@pec.it<li>
+          <li>Tel: +39 0874 4981<li>
+          <li>Fax: +39 0874 629584<li>
+          <li>Per segnalazioni scrivere a:
+          <li>info@lamolisana.it<li>
+          <li>Per contattare l'area commerciale:
+          <li>commerciale@lamolisana.it<li>
+          <li>Per contattare l'area export:
+          <li>export@lamolisana.it<li>
+        </ul>
+      </div>
+      <div class="prodotti-pastificio">
+        <ul class="prodotti">
+          <li>Il Pastificio</li>
+          <li>Grano decorticato a pietra</li>
+          <li>Il Molise c'è</li>
+          <li>Filiera integrata</li>
+          <li>100 anni di pasta</li>
+          <li>Storia della pasta</li>
+          <li>Spaghetto quadrato</li>
+          <li>Le persone</li>
+        </ul>
+        <ul class="pastificio">
+          <li>Le classiche</li>
+          <li>Le speciali</li>
+          <li>Le integrali</li>
+          <li>Le biologiche</li>
+          <li>Le gluten-free</li>
+          <li>Le extra-lusso</li>
+          <li>Le semole</li>
+        </ul>
+        <div class="chef-collection-container">
+          <ul class="chef-collection">
+            <li>Collezione da chef</li>
+            <li>Grandi cucine</li>
+            <li>Biologiche</li>
+            <li>Quadrate</li>
+          </ul>
+        </div>
+
+      </div>
+    </footer>
+    <!-- /footer -->
   </body>
 </html>
